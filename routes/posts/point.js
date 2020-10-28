81279d700dc33cdae843014e637b1fcfdbb6a8aa
Fran Jurmanović <fjurma12@outlook.com>
2020-10-28 22:50:58 +0100

    [SCHNIT-37] - can't point if comments are deleted

--- routes/posts/point.js
+++ routes/posts/point.js
@@ -5,7 +5,6 @@ const Post = require("../../model/Post");
 
 const auth = require("../../middleware/auth");
 const User = require("../../model/User");
-const { Query } = require("mongoose");
 
 router.put('/:postId', auth, async (req, res) => {
     const {user: {id}, body: {type, commentId, replyId}, params: {postId}} = req;
@@ -13,16 +12,27 @@ router.put('/:postId', auth, async (req, res) => {
         if(req.user.id === "anonymous") return res.send("Could not add point");
     	let pst = null;
         if(type === "post") {
-            pst = await Post.findOneAndUpdate({"_id": postId, 'points.userId': {$ne: id}}, { $addToSet: { points: { "userId": id } }});
-            await User.findOneAndUpdate({"_id": pst.userId}, { $inc: {"points": 1 }});
+            pst = await Post.findOne({"_id": postId, 'points.userId': {$ne: id}});
+            if(pst) await Promise.all([
+                Post.findOneAndUpdate({"_id": postId, 'points.userId': {$ne: id}}, { $addToSet: { points: { "userId": id } }}), 
+                User.findOneAndUpdate({"_id": pst.userId}, { $inc: {"points": 1 }})
+            ]);
         } else if (type === "comment") {
-            pst = await Post.findOneAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': {$ne: id} }} }, { $addToSet: { 'comments.$[comment].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }] });
-            let uid = pst.comments.filter(x => x["_id"] == commentId)[0].userId;
-            await User.findOneAndUpdate({"_id": uid}, { $inc: {"points": 1 }});
+            pst = await Post.findOne({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': {$ne: id} }} });
+            let filteredPost = pst.comments.filter(x => x["_id"] == commentId)[0].userId;
+            let uid = filteredPost.userId;
+            if(filteredPost && !filteredPost.isDeleted) await Promise.all([
+                Post.findOneAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': {$ne: id} }} }, { $addToSet: { 'comments.$[comment].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }] }),
+                User.findOneAndUpdate({"_id": uid}, { $inc: {"points": 1 }})
+            ]);
         } else if (type === "reply") {
-            pst = await Post.findByIdAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': {$ne: id} } } }}}, { $addToSet: { 'comments.$[comment].reply.$[repl].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }, { 'repl._id': replyId }] });
-            let uid = pst.comments.filter(x => x["_id"] == commentId)[0].reply.filter(x => x["_id"] == replyId)[0].userId;
-            await User.findOneAndUpdate({"_id": uid}, { $inc: {"points": 1 }});
+            pst = await Post.findOne({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': {$ne: id} } } }}});
+            let filteredPost = pst.comments.filter(x => x["_id"] == commentId)[0].reply.filter(x => x["_id"] == replyId)[0];
+            let uid = filteredPost.userId;
+            if(filteredPost && !filteredPost.isDeleted) await Promise.all([
+                Post.findByIdAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': {$ne: id} } } }}}, { $addToSet: { 'comments.$[comment].reply.$[repl].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }, { 'repl._id': replyId }] }),
+                User.findOneAndUpdate({"_id": uid}, { $inc: {"points": 1 }})
+            ]);
         } else {
             res.send("Could not add point");
             return;
@@ -42,18 +52,34 @@ router.delete("/:postId", auth, async (req, res) => {
     const {user: {id}, body: {type, commentId, replyId}, params: {postId}} = req;
     try {
         if(req.user.id === "anonymous") return res.send("Could not remove point");
+        let pst = null;
         if(type === "post") {
-            pst = await Post.findOneAndUpdate({"_id": postId, 'points.userId': id}, { $pull: { points: { "userId": id } }});
-            await User.findOneAndUpdate({"_id": pst.userId}, { $inc: {"points": -1 }});
+            pst = await Post.findOne({"_id": postId, 'points.userId': id});
+            if (pst) await Promise.all([
+                Post.findOneAndUpdate({"_id": postId, 'points.userId': id}, { $pull: { points: { "userId": id } }}),
+                User.findOneAndUpdate({"_id": pst.userId}, { $inc: {"points": -1 }})
+            ]);
         } else if (type === "comment") {
-            pst = await Post.findOneAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': id }} }, { $pull: { 'comments.$[comment].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }] });
-            let uid = pst.comments.filter(x => x["_id"] == commentId)[0].userId;
-            await User.findOneAndUpdate({"_id": uid}, { $inc: {"points": -1 }});
+            pst = await Post.findOne({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': id, "isDeleted": {$ne: true} }} });
+            let filteredPost = pst.comments.filter(x => x["_id"] == commentId)[0].userId;
+            let uid = filteredPost.userId;
+            if (filteredPost && !filteredPost.isDeleted) await Promise.all([
+                Post.findOneAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, 'points.userId': id, "isDeleted": {$ne: true} }} }, { $pull: { 'comments.$[comment].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }] }),
+                User.findOneAndUpdate({"_id": uid}, { $inc: {"points": -1 }})
+            ]);
         } else if (type === "reply") {
-            pst = await Post.findByIdAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': id } } }}}, { $pull: { 'comments.$[comment].reply.$[repl].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }, { 'repl._id': replyId }] });
-            let uid = pst.comments.filter(x => x._id == commentId)[0].reply.filter(x => x._id == replyId)[0].userId;
-            await User.findOneAndUpdate({"_id": uid}, { $inc: {"points": -1 }});
+            pst = await Post.findByIdAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': id } } }}});
+            let filteredPost = pst.comments.filter(x => x._id == commentId)[0].reply.filter(x => x._id == replyId)[0].userId;
+            let uid = filteredPost.userId;
+            if (filteredPost && !filteredPost.isDeleted) await Promise.all([
+                Post.findByIdAndUpdate({"_id": postId, comments: {$elemMatch: { "_id": commentId, reply: {$elemMatch: { "_id": replyId, 'points.userId': id } } }}}, { $pull: { 'comments.$[comment].reply.$[repl].points': {"userId": id} } }, { arrayFilters: [{ 'comment._id': commentId }, { 'repl._id': replyId }] }),
+                User.findOneAndUpdate({"_id": uid}, { $inc: {"points": -1 }})
+            ]);
         } else {
+            res.send("Could not remove point");
+            return;
+        }
+        if (!pst) {
             res.send("Could not remove point");
             return;
         }
