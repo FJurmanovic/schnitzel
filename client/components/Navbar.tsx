b23de2e0a19d3cd915fd1bd0aa0c58d070c21775
Fran Jurmanović <fjurma12@outlook.com>
2021-10-11 23:13:23 +0200

    app fix

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,131 +1,190 @@
-import React, { Component, useState } from 'react';
-import { observer, inject } from 'mobx-react';
-import {Link, withRouter} from 'react-router-dom';
-import { getRandomInt, getTheme } from '../common/js';
+import React, { Component, useState } from "react";
+import { observer, inject } from "mobx-react";
+import { Link, withRouter } from "react-router-dom";
+import { getRandomInt, getTheme } from "../common/js";
 
-import {Switch, SearchComponent} from './';
-import {SwitchStore, SearchStore} from '../stores';
+import { Switch, SearchComponent } from "./";
+import { SwitchStore, SearchStore } from "../stores";
 
 const searchStore = new SearchStore("Search", 0);
 
 const themeSwitch = new SwitchStore(getTheme("getBool"), () => {
-  getTheme("toggle");
+    getTheme("toggle");
 });
 
 interface NavbarProps {
-  AuthStore: any,
-  location: any
+    AuthStore: any;
+    location: any;
 }
 
 const Logo = (props) => {
-  const logos = ["chicken", "hamburger", "beef", "salad"];
-  const [current, setCurrent] = useState(getRandomInt(0,3));
-  let random = () => {
-    let rnd = getRandomInt(0,3);
-    if (rnd == current) return random();
-    return rnd;
-  };
-  React.useEffect(() => {
-    setCurrent(random);
-  }, [props.change]);
+    const logos = ["chicken", "hamburger", "beef", "salad"];
+    const [current, setCurrent] = useState(getRandomInt(0, 3));
+    let random = () => {
+        let rnd = getRandomInt(0, 3);
+        if (rnd == current) return random();
+        return rnd;
+    };
+    React.useEffect(() => {
+        setCurrent(random);
+    }, [props.change]);
 
-  return(
-    <img src={`/api/logos/${logos[current]}.svg`} className="logo"></img>
-  )
-}
+    return (
+        <img
+            src={`https://api.jurmanovic.com/schnitzel/api/logos/${logos[current]}.svg`}
+            className="logo"
+        ></img>
+    );
+};
 
-
-const NotLoggedLink = () =>
+const NotLoggedLink = () => (
     <>
-      <div className="header-item mr-5">
-          <Link to="/explore" className="btn btn-white btn-rounder">Explore</Link>
-      </div>
-      <div className="header-item">
-        <Link to="/login" className="header-link py-5px">
-          Login
-        </Link>
-      </div>
-      <div className="header-item">
-        <Link to="/register" className="header-link py-5px">
-          Register
-        </Link>
-      </div>
+        <div className="header-item mr-5">
+            <Link to="/explore" className="btn btn-white btn-rounder">
+                Explore
+            </Link>
+        </div>
+        <div className="header-item">
+            <Link to="/login" className="header-link py-5px">
+                Login
+            </Link>
+        </div>
+        <div className="header-item">
+            <Link to="/register" className="header-link py-5px">
+                Register
+            </Link>
+        </div>
     </>
+);
 
-  const LoggedLink = observer(({userData, toggleMenu}) =>
+const LoggedLink = observer(({ userData, toggleMenu }) => (
     <>
-      <div className="header-item">
-        <SearchComponent 
-          store={searchStore}
-        />
-      </div>
-      <div className="header-item mr-5 explore-btn">
-          <Link to="/explore" className="btn btn-white btn-rounder explore-btn">Explore</Link>
-      </div>
-      <div className="header-item">
-        <details className="header-dropdown" id="profile">
-          <summary className="btn btn-default px-7 header-button" onClick={(event) => {
-            event.stopPropagation();
-            toggleMenu(event);
-          }}>
-            <span className="big-screen">{userData.username}</span>
-            <i className="arrow big-screen"></i>
-            <div className="sml-photo mx-auto text-center">
-              {userData.hasPhoto ? <img src={userData.url} className="card-img-top" /> : <img src="https://storage.googleapis.com/schnitzel/default.jpg" className="card-img-top" />}
-            </div>
-          </summary>
-          <ul className="header-dropdown-menu dropdown-menu-dark">
-            <Link to="/explore" className="dropdown-item explore-btn-small">Explore</Link>
-            <li className="dropdown-divider explore-btn-small"></li>
-            <Link to="/profile" className="dropdown-item">View Profile</Link>
-            <Link to="/profile/edit" className="dropdown-item">Edit Profile</Link>
-            <li className="dropdown-divider"/>
-            <div className="dropdown-item noselect" onClick={(event) => {
-              event.stopPropagation();
-              themeSwitch.toggleClick();
-              }}>
-              <Switch 
-                store={themeSwitch}
-              /> 
-              <span>Dark mode</span></div>
-            <Link to="/logout" className="dropdown-item">Logout</Link>
-          </ul>
-        </details>
-      </div>
+        <div className="header-item">
+            <SearchComponent store={searchStore} />
+        </div>
+        <div className="header-item mr-5 explore-btn">
+            <Link
+                to="/explore"
+                className="btn btn-white btn-rounder explore-btn"
+            >
+                Explore
+            </Link>
+        </div>
+        <div className="header-item">
+            <details className="header-dropdown" id="profile">
+                <summary
+                    className="btn btn-default px-7 header-button"
+                    onClick={(event) => {
+                        event.stopPropagation();
+                        toggleMenu(event);
+                    }}
+                >
+                    <span className="big-screen">{userData.username}</span>
+                    <i className="arrow big-screen"></i>
+                    <div className="sml-photo mx-auto text-center">
+                        {userData.hasPhoto ? (
+                            <img src={userData.url} className="card-img-top" />
+                        ) : (
+                            <img
+                                src="https://storage.googleapis.com/schnitzel/default.jpg"
+                                className="card-img-top"
+                            />
+                        )}
+                    </div>
+                </summary>
+                <ul className="header-dropdown-menu dropdown-menu-dark">
+                    <Link
+                        to="/explore"
+                        className="dropdown-item explore-btn-small"
+                    >
+                        Explore
+                    </Link>
+                    <li className="dropdown-divider explore-btn-small"></li>
+                    <Link to="/profile" className="dropdown-item">
+                        View Profile
+                    </Link>
+                    <Link to="/profile/edit" className="dropdown-item">
+                        Edit Profile
+                    </Link>
+                    <li className="dropdown-divider" />
+                    <div
+                        className="dropdown-item noselect"
+                        onClick={(event) => {
+                            event.stopPropagation();
+                            themeSwitch.toggleClick();
+                        }}
+                    >
+                        <Switch store={themeSwitch} />
+                        <span>Dark mode</span>
+                    </div>
+                    <Link to="/logout" className="dropdown-item">
+                        Logout
+                    </Link>
+                </ul>
+            </details>
+        </div>
     </>
-  );
+));
 @inject("AuthStore")
 @observer
 class Navbar extends Component<NavbarProps> {
     render() {
         return (
-          <>
-            <header className="header border-bottom border-black p-5 f4">
-                <div className={this.props.AuthStore.isAuth ? "header-item--full" : "header-item--full"}>
-                    <Link to="/">
-                      <Logo change={this.props.location.key} />
-                    </Link>
-                </div>
-                { this.props.AuthStore.isAuth ? <LoggedLink userData={this.props.AuthStore.userData} toggleMenu={this.props.AuthStore.toggleMenu} /> : <NotLoggedLink /> }
-            </header>
-            {this.props.AuthStore.isAuth && this.props.AuthStore.menuShow && <div className="header-small">
-              <ul className="header-dropdown-menu dropdown-menu-dark">
-                <Link to="/explore" className="dropdown-item explore-btn-small">Explore</Link>
-                <li className="dropdown-divider explore-btn-small"></li>
-                <Link to="/profile" className="dropdown-item">View Profile</Link>
-                <Link to="/profile/edit" className="dropdown-item">Edit Profile</Link>
-                <div className="dropdown-item noselect" onClick={(event) => {
-                  event.stopPropagation();
-                  themeSwitch.toggleClick();
-                  }}>
-                  <Switch 
-                    store={themeSwitch}
-                  /> 
-                  <span>Dark mode</span></div>
-                <Link to="/logout" className="dropdown-item">Logout</Link>
-              </ul>
-            </div>}
-          </>
+            <>
+                <header className="header border-bottom border-black p-5 f4">
+                    <div
+                        className={
+                            this.props.AuthStore.isAuth
+                                ? "header-item--full"
+                                : "header-item--full"
+                        }
+                    >
+                        <Link to="/">
+                            <Logo change={this.props.location.key} />
+                        </Link>
+                    </div>
+                    {this.props.AuthStore.isAuth ? (
+                        <LoggedLink
+                            userData={this.props.AuthStore.userData}
+                            toggleMenu={this.props.AuthStore.toggleMenu}
+                        />
+                    ) : (
+                        <NotLoggedLink />
+                    )}
+                </header>
+                {this.props.AuthStore.isAuth && this.props.AuthStore.menuShow && (
+                    <div className="header-small">
+                        <ul className="header-dropdown-menu dropdown-menu-dark">
+                            <Link
+                                to="/explore"
+                                className="dropdown-item explore-btn-small"
+                            >
+                                Explore
+                            </Link>
+                            <li className="dropdown-divider explore-btn-small"></li>
+                            <Link to="/profile" className="dropdown-item">
+                                View Profile
+                            </Link>
+                            <Link to="/profile/edit" className="dropdown-item">
+                                Edit Profile
+                            </Link>
+                            <div
+                                className="dropdown-item noselect"
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    themeSwitch.toggleClick();
+                                }}
+                            >
+                                <Switch store={themeSwitch} />
+                                <span>Dark mode</span>
+                            </div>
+                            <Link to="/logout" className="dropdown-item">
+                                Logout
+                            </Link>
+                        </ul>
+                    </div>
+                )}
+            </>
         );
     }
 }
--- client/pages/Register.tsx
+++ client/pages/Register.tsx
@@ -1,73 +1,72 @@
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
-import {withRouter} from 'react-router-dom';
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+import { withRouter } from "react-router-dom";
 
-import {FormComponent, InputComponent, RadioComponent, FormGroupComponent, DropdownComponent} from '../components';
+import {
+    FormComponent,
+    InputComponent,
+    RadioComponent,
+    FormGroupComponent,
+    DropdownComponent,
+} from "../components";
 
-import {FormsService} from '../services';
+import { FormsService } from "../services";
 const fields = [
     {
         name: "username",
         type: "text",
-        rules: "required|string|between:1,10"
+        rules: "required|string|between:1,10",
     },
     {
         name: "email",
         type: "email",
-        rules: "required|string|between:1,50"
+        rules: "required|string|between:1,50",
     },
     {
         name: "emailConfirm",
         type: "email",
-        rules: "required|string|same:email"
+        rules: "required|string|same:email",
     },
     {
         name: "password",
         type: "password",
-        rules: "required|between: 3,15"
+        rules: "required|between: 3,15",
     },
     {
         name: "passwordConfirm",
         type: "password",
-        rules: "required|same:password"
+        rules: "required|same:password",
     },
     {
-      name: "privacy",
-      type: "radio",
-      rules: "required",
-      default: "Private",
-      value: "Private"
+        name: "privacy",
+        type: "radio",
+        rules: "required",
+        default: "Private",
+        value: "Private",
     },
-    {
-      name: "categories",
-      type: "checkbox",
-      rules: "required",
-      value: []
-    }
-]
+];
 
-
-const forms:any = new FormsService({fields});
+const forms: any = new FormsService({ fields });
 
 type RegisterProps = {
-  RegisterStore: any;
-  history: any;
-}
+    RegisterStore: any;
+    history: any;
+};
 
 @inject("RegisterStore")
 @observer
 class Register extends Component<RegisterProps> {
     hooks: any;
     constructor(props) {
-      super(props);
-      this.hooks = {
-        onSuccess(form) {
-          props.RegisterStore.submitClick(form.values(), props.history);
-        },
-        onError(form) {
-          console.log(form.values())
-        } 
-      }
+        super(props);
+        this.hooks = {
+            onSuccess(form) {
+                props.RegisterStore.submitClick(form.values(), props.history);
+            },
+            onError(form) {
+                console.log(form.values());
+            },
+        };
     }
     componentWillUnmount() {
         forms.reset();
@@ -75,21 +74,56 @@ class Register extends Component<RegisterProps> {
 
     render() {
         return (
-          <div className="text-center">
-            <FormComponent className="mx-auto col-7 f4" form={forms} onSubmit={(e) => forms.onSubmit(e, this.hooks)} onCancel={() => this.props.history.push("/")}>
-                <InputComponent className="width-full f5 py-2 my-2" message="Username (1-10 characters): " errorMessage="Username must have between 1 and 10 characters." name="username" autoFocus />
-                <InputComponent className="width-full f5 py-2 my-2" message="Email: " errorMessage="Wrong email format." name="email" />
-                <InputComponent className="width-full f5 py-2 my-2" message="Confirm Email: " errorMessage="The e-mails do not match." name="emailConfirm" />
-                <FormGroupComponent className="my-2">
-                  <div>Profile privacy:</div>
-                  <RadioComponent name="privacy" value="Private" />
-                  <RadioComponent name="privacy" value="Public" />
-                </FormGroupComponent>
-                <InputComponent className="width-full f5 py-2 my-2" message="Password (3-15 characters): " errorMessage="Password must be between 3 and 15 characters." name="password" />
-                <InputComponent className="width-full f5 py-2 my-2" message="Confirm Password: " errorMessage="Passwords do not match." name="passwordConfirm" />
-            </FormComponent>
-            {this.props.RegisterStore.errorMessage && <div className="mx-auto col-7 h4 text-red">{this.props.RegisterStore.errorMessage}</div>}
-          </div>
+            <div className="text-center">
+                <FormComponent
+                    className="mx-auto col-7 f4"
+                    form={forms}
+                    onSubmit={(e) => forms.onSubmit(e, this.hooks)}
+                    onCancel={() => this.props.history.push("/")}
+                >
+                    <InputComponent
+                        className="width-full f5 py-2 my-2"
+                        message="Username (1-10 characters): "
+                        errorMessage="Username must have between 1 and 10 characters."
+                        name="username"
+                        autoFocus
+                    />
+                    <InputComponent
+                        className="width-full f5 py-2 my-2"
+                        message="Email: "
+                        errorMessage="Wrong email format."
+                        name="email"
+                    />
+                    <InputComponent
+                        className="width-full f5 py-2 my-2"
+                        message="Confirm Email: "
+                        errorMessage="The e-mails do not match."
+                        name="emailConfirm"
+                    />
+                    <FormGroupComponent className="my-2">
+                        <div>Profile privacy:</div>
+                        <RadioComponent name="privacy" value="Private" />
+                        <RadioComponent name="privacy" value="Public" />
+                    </FormGroupComponent>
+                    <InputComponent
+                        className="width-full f5 py-2 my-2"
+                        message="Password (3-15 characters): "
+                        errorMessage="Password must be between 3 and 15 characters."
+                        name="password"
+                    />
+                    <InputComponent
+                        className="width-full f5 py-2 my-2"
+                        message="Confirm Password: "
+                        errorMessage="Passwords do not match."
+                        name="passwordConfirm"
+                    />
+                </FormComponent>
+                {this.props.RegisterStore.errorMessage && (
+                    <div className="mx-auto col-7 h4 text-red">
+                        {this.props.RegisterStore.errorMessage}
+                    </div>
+                )}
+            </div>
         );
     }
 }
